ef8ccf50e0985a993f42240f19c7b56c1daf1699
clivegreen-china2 <clivegreen.china@outlook.com>
2024-11-16 07:32:15 +0800

    Simplified StringStats.

--- code/hangman/string_stats.py
+++ code/hangman/string_stats.py
@@ -4,32 +4,12 @@ class StringStats:
     def __init__(self, text_string: str):
         self.text_string_original = text_string
         self.word_list: [str] = re.findall(r'\w+', text_string.lower())
+        self.text_string: str = ' '.join(self.word_list)
         self.word_count: int = len(self.word_list)
         self.word_lengths: [int] = [len(w) for w in self.word_list]
-        self.text_string: str = ' '.join(self.word_list)
-        self.all_letters_string: str = ''.join(self.word_list)
-        self.all_letters_count: int = len(self.all_letters_string)
-        self.all_letters_list: [str] = list(self.all_letters_string)
-        self.unique_letters_list: [str] = sorted(list(set(self.all_letters_list)))
-        self.unique_letters_count: int = len(self.unique_letters_list)
-        self.letter_indices: dict[str:[int]] = {c: [] for c in self.unique_letters_list}
-        for index, letter in enumerate(self.all_letters_list):
+        self.letter_count = sum(self.word_lengths)
+        self.unique_letters = sorted(list(set(''.join(self.word_list))))
+        self.unique_letter_count = len(self.unique_letters)
+        self.letter_indices: dict[str:[int]] = {c: [] for c in self.unique_letters}
+        for index, letter in enumerate(self.text_string):
             self.letter_indices[letter].append(index)
-
-    def __iter__(self):
-        return iter([
-            ('text_string_original', self.text_string_original),
-            ('word_list', self.word_list),
-            ('word_count', self.word_count),
-            ('word_lengths', self.word_lengths),
-            ('text_string', self.text_string),
-            ('all_letters_string', self.all_letters_string),
-            ('all_letters_count', self.all_letters_count),
-            ('all_letters_list', self.all_letters_list),
-            ('unique_letters_list', self.unique_letters_list),
-            ('unique_letters_count', self.unique_letters_count),
-            ('letter_indices', self.letter_indices)
-        ])
-
-    def show(self):
-        [print(f'{pair[0]}: {pair[1]}') for pair in self]
