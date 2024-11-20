c8d901a22c6a08b6541d0b08cb3c89b0b8c4ffa8
clivegreen-china2 <clivegreen.china@outlook.com>
2024-11-20 18:40:32 +0800

    Various revisions.

---
+++ code/bubble_sort/main.py
@@ -0,0 +1,18 @@
+from math import prod
+from get_numbers import get_sorted_numbers
+
+# sub_count cannot be larger than count:
+count: int = 3
+sub_count: int = 2
+
+sorted_numbers: [int] = get_sorted_numbers(count)
+start_from: int = count - sub_count
+factors: [int] = sorted_numbers[start_from::]
+product: int = prod(factors)
+
+factors_as_text: str = ", ".join([str(n) for n in factors])
+product_as_text: str = f'{product:,}'
+print(
+    f'\nThe product of the {sub_count} largest numbers '
+    f'({factors_as_text}) is {product_as_text}.'
+)
